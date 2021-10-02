a58822f9f14384a8eb3d6ecb2714df1f4e233b83
Ilya Ballet <anonimus33@protonmail.com>
2021-10-02 15:29:02 +0300

    fixed batch build file

--- build-windows.bat
+++ build-windows.bat
@@ -1,9 +1,7 @@
-mkdir build
+if not exist "build" mkdir "build"
 del /s /q build\*
 
-c++ -std=c++17 -pedantic-errors -O3 -pthread src\client\*.cpp -o build\surv-royale-client -Izipper -Iwin\include -Lwin\lib \
--lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lZipper -lz # -Wall -Wextra
-
+c++ -std=c++17 -pedantic-errors -O3 -pthread src\client\*.cpp -o build\surv-royale-client -Izipper -Iwin\include -Lwin\lib -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lZipper -lz
 c++ -std=c++17 -pedantic-errors -O3 -pthread src\server\*.cpp -o build\surv-royale-server -Iwin\include -Lwin\lib -lsfml-network-2 -lsfml-system-2
 
 7z a build\GameData.zip GameData
