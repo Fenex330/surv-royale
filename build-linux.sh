73a0dd70c010cc832fbe2d6881d8c53df166543a
Ilya Ballet <anonimus33@protonmail.com>
2021-10-02 22:00:44 +0300

    quick formatting

--- build-linux.sh
+++ build-linux.sh
@@ -2,9 +2,7 @@ mkdir -p build
 rm -rf build
 mkdir build
 
-g++ -std=c++17 -pedantic-errors -O3 -pthread src/client/*.cpp -o build/surv-royale-client -Izipper -Lzipper \
--lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -lZipper-static -lz # -Wall -Wextra
-
+g++ -std=c++17 -pedantic-errors -O3 -pthread src/client/*.cpp -o build/surv-royale-client -Izipper -Lzipper -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -lZipper-static -lz # -Wall -Wextra
 g++ -std=c++17 -pedantic-errors -O3 -pthread src/server/*.cpp -o build/surv-royale-server -lsfml-network -lsfml-system
 
 7z a build/GameData.zip GameData
