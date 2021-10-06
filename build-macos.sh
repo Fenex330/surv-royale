439ac1857226ff0c045c53993e2d3ac5bb7d252b
Ilya Ballet <anonimus33@protonmail.com>
2021-10-06 19:47:02 +0300

    updated build-macos.sh

--- build-macos.sh
+++ build-macos.sh
@@ -2,8 +2,8 @@ mkdir -p build
 rm -rf build
 mkdir build
 
-clang++ -std=c++17 -pedantic-errors -O3 -pthread src/client/*.cpp -o build/surv-royale-client -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
-clang++ -std=c++17 -pedantic-errors -O3 -pthread src/server/*.cpp -o build/surv-royale-server -lsfml-network -lsfml-system
+clang++ -std=c++17 -pedantic-errors -O3 -pthread src/client/*.cpp -o build/surv-royale-client -F macos/frameworks -rpath @executable_path/ -framework sfml-graphics -framework sfml-window -framework sfml-audio -framework sfml-network -framework sfml-system
+clang++ -std=c++17 -pedantic-errors -O3 -pthread src/server/*.cpp -o build/surv-royale-server -F macos/frameworks -rpath @executable_path/ -framework sfml-network -framework sfml-system
 
 tar -cf build/GameData.tar GameData
 
