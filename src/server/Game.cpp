86b515998d67482c9dfd9467da2fd85488f62bdf
Ilya Ballet <anonimus33@protonmail.com>
2021-11-06 17:38:12 +0200

    added multiplayer test

--- src/server/Game.cpp
+++ src/server/Game.cpp
@@ -9,6 +9,13 @@ Game::Game()
     UDPsocket.setBlocking(false);
     TCPsocket.setBlocking(false);
 
+    // REMOVE
+    x = 0;
+    y = 0;
+    rotation = 0.0;
+    address = "127.0.0.1";
+    port = surv::DEFAULT_PORT;
+
     if (UDPsocket.bind(surv::DEFAULT_PORT) != sf::Socket::Done)
         exit(1);
 }
@@ -17,6 +24,41 @@ void Game::run()
 {
     while (!quit)
     {
-        //
+        sf::Packet packet;
+
+        receive(packet);
+        sendPlayersList(packet);
+    }
+}
+
+void Game::sendPlayersList(sf::Packet packet)
+{
+    packet << static_cast<sf::Uint8>(NetCodes::PlayersList) << x << y << rotation;
+    assert(packet.getDataSize() <= sf::UdpSocket::MaxDatagramSize);
+
+    if (UDPsocket.send(packet, address, port) != sf::Socket::Done)
+        exit(1);
+}
+
+void Game::receive(sf::Packet packet)
+{
+    if (UDPsocket.receive(packet, address, port) != sf::Socket::Done)
+        return;
+
+    sf::Uint8 netcode_raw;
+    packet >> netcode_raw;
+    NetCodes netcode = static_cast<NetCodes>(netcode_raw);
+
+    switch (netcode)
+    {
+        case NetCodes::MoveAndRotate:
+            if (packet /*>> netcode_raw*/ >> x >> y >> rotation) {}
+            else
+                exit(1);
+
+            break;
+
+        default:
+            break;
     }
 }
--- src/server/Game.hpp
+++ src/server/Game.hpp
@@ -12,8 +12,17 @@ public:
     sf::TcpSocket TCPsocket;
     //std::map<std::string, Player> players;
 
+    // REMOVE
+    sf::Int8 x;
+    sf::Int8 y;
+    double rotation;
+    sf::IpAddress address;
+    unsigned short port;
+
     static bool quit;
 
     Game();
     void run();
+    void sendPlayersList(sf::Packet packet);
+    void receive(sf::Packet packet);
 };
