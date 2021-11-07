89ee11ce8fdd77e50eac7e8b1498bca4e1cfefbb
Ilya Ballet <anonimus33@protonmail.com>
2021-11-07 15:34:01 +0200

    update headers.hpp

--- src/client/headers.hpp
+++ src/client/headers.hpp
@@ -3,13 +3,20 @@
 #include <utility>
 #include <string>
 #include <map>
+
 #include <cassert>
 #include <cstdlib>
 #include <cmath>
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 #include "../config.hpp"
 #include "dxTarRead.h"
+
 #include "Player.hpp"
 #include "MainPlayer.hpp"
 #include "Game.hpp"
