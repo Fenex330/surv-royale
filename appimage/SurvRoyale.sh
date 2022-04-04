5433b326ea122cdb9d844b719f5194a339035c30
Ilya Ballet <anonimus33@protonmail.com>
2022-04-05 01:13:25 +0300

    refactoring

--- appimage/SurvRoyale.sh
+++ appimage/SurvRoyale.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [$1 == "--server"] then
-    exec surv-royale-server
+if [[ $1 == "--server" ]]
+then
+    exec ./surv-royale-server
 else
-    exec surv-royale-client
+    exec ./surv-royale-client
 fi
