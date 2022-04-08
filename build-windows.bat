be292b6de25a24675e7dbce10a9b52d4cba97289
Ilya Ballet <anonimus33@protonmail.com>
2022-04-08 21:23:12 +0300

    fixed batch file errors 5

--- build-windows.bat
+++ build-windows.bat
@@ -1,7 +1,7 @@
 if not exist "build" mkdir "build"
 del /s /q build\*
 
-FOR /F "tokens=*" %%g IN ('git describe --abbrev=100') do (SET VAR=%%g)
+FOR /F %%i IN ('git describe --abbrev=100') DO set VAR=%%i
 
 g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=VAR src\client\*.cpp src\client\imgui\*.cpp -o build\surv-royale-client -I win\include -L win\lib -mwindows -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lopengl32
 g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=VAR src\server\*.cpp -o build\surv-royale-server -I win\include -L win\lib -lsfml-network-2 -lsfml-system-2
