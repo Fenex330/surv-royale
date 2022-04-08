42d07575b4704bca77bfd36be3b73cc1321e08a3
Ilya Ballet <anonimus33@protonmail.com>
2022-04-08 20:51:32 +0300

    added commit check support for Windows

--- build-windows.bat
+++ build-windows.bat
@@ -1,8 +1,8 @@
 if not exist "build" mkdir "build"
 del /s /q build\*
 
-g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread src\client\*.cpp src\client\imgui\*.cpp -o build\surv-royale-client -I win\include -L win\lib -mwindows -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lopengl32
-g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread src\server\*.cpp -o build\surv-royale-server -I win\include -L win\lib -lsfml-network-2 -lsfml-system-2
+g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=\"$(git describe --abbrev=100)\" src\client\*.cpp src\client\imgui\*.cpp -o build\surv-royale-client -I win\include -L win\lib -mwindows -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lopengl32
+g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=\"$(git describe --abbrev=100)\" src\server\*.cpp -o build\surv-royale-server -I win\include -L win\lib -lsfml-network-2 -lsfml-system-2
 
 7z a build\GameData.tar GameData
 
