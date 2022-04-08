023a2c967ef8876b2e6ba531f3ee647edf9b41a0
Ilya Ballet <anonimus33@protonmail.com>
2022-04-08 21:01:12 +0300

    fixed batch file errors 2

--- build-windows.bat
+++ build-windows.bat
@@ -1,12 +1,12 @@
 if not exist "build" mkdir "build"
 del /s /q build\*
 
-cmd > tmp
-set /p myvar = < tmp
+git describe --abbrev=100 > tmp
+set /p version = < tmp
 del tmp
 
-g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=\"$(git describe --abbrev=100)\" src\client\*.cpp src\client\imgui\*.cpp -o build\surv-royale-client -I win\include -L win\lib -mwindows -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lopengl32
-g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=\"$(git describe --abbrev=100)\" src\server\*.cpp -o build\surv-royale-server -I win\include -L win\lib -lsfml-network-2 -lsfml-system-2
+g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=version src\client\*.cpp src\client\imgui\*.cpp -o build\surv-royale-client -I win\include -L win\lib -mwindows -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lopengl32
+g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=version src\server\*.cpp -o build\surv-royale-server -I win\include -L win\lib -lsfml-network-2 -lsfml-system-2
 
 7z a build\GameData.tar GameData
 
