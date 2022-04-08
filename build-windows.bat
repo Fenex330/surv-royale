8e8175bf7d4844a07a8e64efea72d33cf6bb9050
Ilya Ballet <anonimus33@protonmail.com>
2022-04-08 20:56:56 +0300

    fixed batch file errors

--- build-windows.bat
+++ build-windows.bat
@@ -1,6 +1,10 @@
 if not exist "build" mkdir "build"
 del /s /q build\*
 
+cmd > tmp
+set /p myvar = < tmp
+del tmp
+
 g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=\"$(git describe --abbrev=100)\" src\client\*.cpp src\client\imgui\*.cpp -o build\surv-royale-client -I win\include -L win\lib -mwindows -lsfml-graphics-2 -lsfml-window-2 -lsfml-audio-2 -lsfml-network-2 -lsfml-system-2 -lopengl32
 g++ -std=c++17 -pedantic-errors -O3 -DNDEBUG -pthread -DGAME_VERSION=\"$(git describe --abbrev=100)\" src\server\*.cpp -o build\surv-royale-server -I win\include -L win\lib -lsfml-network-2 -lsfml-system-2
 
