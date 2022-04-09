4ab045c2a24094d872b3f50bdc6928f723a48c42
Ilya Ballet <anonimus33@protonmail.com>
2022-04-09 03:39:27 +0300

    added parallel build to brew

--- surv-royale.rb
+++ surv-royale.rb
@@ -5,10 +5,12 @@ class SurvRoyale < Formula
   sha256 "e2977edff3e1b855845230695839f16a055c6a952de2fbd9d7d9b733ca8629b6"
   license "MIT"
   head "https://github.com/Fenex330/surv-royale.git", branch: "master"
+
   depends_on "sfml"
+  depends_on "parallel" => :build
 
   def install
-    system "make"
+    system "make", "release", "client", "server"
     system "make", "install"
   end
 end
