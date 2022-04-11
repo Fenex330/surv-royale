fd12dd764ba5096e9cfb14ea94638ae6ee8bfe2b
Ilya Ballet <anonimus33@protonmail.com>
2022-04-11 10:47:33 +0300

    updated surv-royale.rb

--- surv-royale.rb
+++ surv-royale.rb
@@ -1,20 +1,14 @@
 class SurvRoyale < Formula
   desc "Desktop clone of surviv.io written in C++"
   homepage "https://github.com/Fenex330/surv-royale"
-  url "https://github.com/Fenex330/surv-royale/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "e2977edff3e1b855845230695839f16a055c6a952de2fbd9d7d9b733ca8629b6"
+  head "https://github.com/Fenex330/surv-royale.git", tag: "v0.1.0"
   license "MIT"
-  head "https://github.com/Fenex330/surv-royale.git", branch: "master"
-  #head "https://github.com/Fenex330/surv-royale.git", tag: "v0.0.1"
-  #version "0.0.1"
+  version "0.1.0"
 
   depends_on "sfml"
-  #depends_on "parallel" => :build
+  depends_on "parallel" => :build
 
   def install
-    #system "git", "checkout", "v0.0.1"
-    #system "make", "release", "client", "server"
-    system "make"
-    system "make", "install"
+    system "make", "release", "client", "server", "install"
   end
 end
