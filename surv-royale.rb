8f75701ca68a29072d8c259e527cf972b5ec5062
Ilya Ballet <anonimus33@protonmail.com>
2022-04-13 04:03:52 +0300

    makefile added custom prefix for brew build

--- surv-royale.rb
+++ surv-royale.rb
@@ -10,6 +10,6 @@ class SurvRoyale < Formula
 
   def install
     system "make", "release", "client", "server"
-    system "make", "install", "PREFIX=usr"
+    system "make", "install", "PREFIX=/usr"
   end
 end
