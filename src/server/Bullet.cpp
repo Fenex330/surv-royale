3ea3dd1fd840d9bdc3e832fae77d1da96a42db74
Ilya Ballet <anonimus33@protonmail.com>
2022-04-27 18:22:42 +0300

    added more bullet stats

--- src/server/Bullet.cpp
+++ src/server/Bullet.cpp
@@ -5,8 +5,15 @@ Bullet::Bullet(std::string nickname,
                sf::Int16 y,
                double rotation,
                int damage,
+               int range,
                int speed,
                double falloff,
+               double headshot_multiplier;
+               double obstacle_multiplier;
+               double tracer_width;
+               double tracer_length;
+               bool is_suppressed;
+               bool potato_swap;
                std::function<void(Bullet*)> action) : active (true),
                                                       count (0),
                                                       nickname (nickname),
@@ -14,8 +21,15 @@ Bullet::Bullet(std::string nickname,
                                                       y (y),
                                                       rotation (rotation),
                                                       damage (damage),
+                                                      range (range),
                                                       speed (speed),
                                                       falloff (falloff),
+                                                      headshot_multiplier (headshot_multiplier),
+                                                      obstacle_multiplier (obstacle_multiplier),
+                                                      tracer_width (tracer_width),
+                                                      tracer_length (tracer_length),
+                                                      is_suppressed (is_suppressed),
+                                                      potato_swap (potato_swap),
                                                       action (action)
 {
     //
--- src/server/Bullet.hpp
+++ src/server/Bullet.hpp
@@ -10,12 +10,21 @@ class Bullet
         sf::Int16 y;
         int count;
         int damage;
+        int range;
         int speed;
         double rotation;
         double falloff;
+        double headshot_multiplier;
+        double obstacle_multiplier;
+        const double tracer_width;
+        const double tracer_length;
+        const bool is_suppressed;
+        const bool potato_swap;
         const std::string nickname;
         const std::function<void(Bullet*)> action;
 
-        Bullet(std::string nickname, sf::Int16 x, sf::Int16 y, double rotation, int damage, int speed, double falloff, std::function<void(Bullet*)> action);
+        Bullet(std::string nickname, sf::Int16 x, sf::Int16 y, double rotation, int damage, int range, int speed, double falloff, double headshot_multiplier,
+               double obstacle_multiplier, double tracer_width, double tracer_length, bool is_suppressed, bool potato_swap, std::function<void(Bullet*)> action);
+
         void move();
 };
