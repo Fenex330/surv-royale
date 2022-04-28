54200e601636381456a8d2be9a28846e84621fad
Ilya Ballet <anonimus33@protonmail.com>
2022-04-28 19:50:15 +0300

    simplified constructors

--- src/server/Projectile.cpp
+++ src/server/Projectile.cpp
@@ -1,35 +1,5 @@
 #include "headers.hpp"
 
-void Projectile::init(std::string nickname,
-                      std::string gunname,
-                      sf::Int16 x,
-                      sf::Int16 y,
-                      double rotation,
-                      double damage,
-                      double range,
-                      double speed,
-                      double falloff,
-                      double headshot_multiplier,
-                      double obstacle_multiplier,
-                      bool potato_swap,
-                      std::function<void(Projectile*)> action)
-{
-    active = true;
-    this->nickname = nickname;
-    this->gunname = gunname;
-    this->x = x;
-    this->y = y;
-    this->rotation = rotation;
-    this->damage = damage;
-    this->range = range;
-    this->speed = speed;
-    this->falloff = falloff;
-    this->headshot_multiplier = headshot_multiplier;
-    this->obstacle_multiplier = obstacle_multiplier;
-    this->potato_swap = potato_swap;
-    this->action = action;
-}
-
 void Projectile::move()
 {
     // damage = damage * (falloff ^ (distance/100))
--- src/server/Projectile.hpp
+++ src/server/Projectile.hpp
@@ -21,6 +21,33 @@ class Projectile
         std::function<void(Projectile*)> action;
 
         void move();
-        void init(std::string nickname, std::string gunname, sf::Int16 x, sf::Int16 y, double rotation, double damage, double range, double speed, double falloff,
-                  double headshot_multiplier, double obstacle_multiplier, bool potato_swap, std::function<void(Projectile*)> action);
+        void init(const std::string &nickname,
+                  const std::string &gunname,
+                  const sf::Int16 &x,
+                  const sf::Int16 &y,
+                  const double &rotation,
+                  const double &damage,
+                  const double &range,
+                  const double &speed,
+                  const double &falloff,
+                  const double &headshot_multiplier,
+                  const double &obstacle_multiplier,
+                  const bool &potato_swap,
+                  const std::function<void(Projectile*)> &action)
+        {
+            active = true;
+            this->nickname = nickname;
+            this->gunname = gunname;
+            this->x = x;
+            this->y = y;
+            this->rotation = rotation;
+            this->damage = damage;
+            this->range = range;
+            this->speed = speed;
+            this->falloff = falloff;
+            this->headshot_multiplier = headshot_multiplier;
+            this->obstacle_multiplier = obstacle_multiplier;
+            this->potato_swap = potato_swap;
+            this->action = action;
+        }
 };
