8efa86cb17a286a875720e765e6e431e42925f28
Ilya Ballet <anonimus33@protonmail.com>
2022-04-28 15:25:57 +0300

    fixed datatypes

--- src/server/Weapon.hpp
+++ src/server/Weapon.hpp
@@ -15,11 +15,11 @@ class Weapon : public Item
         AmmoType ammo_type;
         sf::Int16 magazine_capacity;
         int burst_count;
-        int stand_spread;
-        int move_spread;
-        int player_damage;
         int unit_range;
-        int Projectile_speed;
+        double stand_spread;
+        double move_spread;
+        double player_damage;
+        double Projectile_speed;
         double firing_rate;
         double recoil_delay;
         double switch_delay;
@@ -33,7 +33,7 @@ class Weapon : public Item
         bool is_throwable;
         std::function<void(Projectile*)> action;
 
-        Weapon(std::string name, Rarity rarity, FiringMode firing_mode, AmmoType ammo_type, sf::Int16 magazine_capacity, int burst_count, int stand_spread, int move_spread, int player_damage,
-               int unit_range, int Projectile_speed, double firing_rate, double recoil_delay, double switch_delay, double reload_time, double Projectile_falloff, double headshot_multiplier,
+        Weapon(std::string name, Rarity rarity, FiringMode firing_mode, AmmoType ammo_type, sf::Int16 magazine_capacity, int burst_count, int unit_range, double stand_spread, double move_spread,
+               double player_damage, int Projectile_speed, double firing_rate, double recoil_delay, double switch_delay, double reload_time, double Projectile_falloff, double headshot_multiplier,
                double obstacle_multiplier, double passive_slowdown, double shooting_slowdown, bool potato_swap = true, bool is_throwable = false, std::function<void(Projectile*)> action = [](Bullet*){});
 };
