7276e4e6d9c00f08c9161b4dddea41d23d7969d3
Ilya Ballet <anonimus33@protonmail.com>
2022-04-28 15:16:54 +0300

    weapon properties reorder

--- src/server/Weapon.hpp
+++ src/server/Weapon.hpp
@@ -14,16 +14,16 @@ class Weapon : public Item
         FiringMode firing_mode;
         AmmoType ammo_type;
         sf::Int16 magazine_capacity;
-        int reload_time;
         int burst_count;
-        int firing_rate;
-        int recoil_delay;
         int stand_spread;
         int move_spread;
-        int switch_delay;
         int player_damage;
         int unit_range;
         int Projectile_speed;
+        double firing_rate;
+        double recoil_delay;
+        double switch_delay;
+        double reload_time;
         double Projectile_falloff;
         double headshot_multiplier;
         double obstacle_multiplier;
@@ -33,7 +33,7 @@ class Weapon : public Item
         bool is_throwable;
         std::function<void(Projectile*)> action;
 
-        Weapon(std::string name, Rarity rarity, FiringMode firing_mode, AmmoType ammo_type, sf::Int16 magazine_capacity, int reload_time, int burst_count, int firing_rate, int recoil_delay,
-               int stand_spread, int move_spread, int switch_delay, int player_damage, int unit_range, int Projectile_speed, double Projectile_falloff, double headshot_multiplier,
+        Weapon(std::string name, Rarity rarity, FiringMode firing_mode, AmmoType ammo_type, sf::Int16 magazine_capacity, int burst_count, int stand_spread, int move_spread, int player_damage,
+               int unit_range, int Projectile_speed, double firing_rate, double recoil_delay, double switch_delay, double reload_time, double Projectile_falloff, double headshot_multiplier,
                double obstacle_multiplier, double passive_slowdown, double shooting_slowdown, bool potato_swap = true, bool is_throwable = false, std::function<void(Projectile*)> action = [](Bullet*){});
 };
