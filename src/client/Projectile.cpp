e77f83e8e109f3b7310cc09070a4ee71aa72c628
Ilya Ballet <anonimus33@protonmail.com>
2022-05-15 16:59:18 +0300

    cleanup of projectile class

--- src/client/Projectile.cpp
+++ src/client/Projectile.cpp
@@ -1,7 +1,9 @@
 #include "headers.hpp"
 
-Projectile::Projectile(sf::Int16 x, sf::Int16 y, double rotation) : x (x), y (y), rotation (rotation)
+Projectile::Projectile(sf::Int16 x, sf::Int16 y, double rotation)
 {
     sprite.setFillColor(sf::Color::White);
     sprite.setSize(sf::Vector2f(surv::PLAYER_RADIUS, 5));
+    sprite.setPosition(x, y);
+    sprite.setRotation(rotation);
 }
--- src/client/Projectile.hpp
+++ src/client/Projectile.hpp
@@ -6,9 +6,5 @@ class Projectile
 {
     public:
         sf::RectangleShape sprite;
-        sf::Int16 x;
-        sf::Int16 y;
-        double rotation;
-
         Projectile(sf::Int16 x, sf::Int16 y, double rotation);
 };
