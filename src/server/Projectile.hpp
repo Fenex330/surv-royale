f26a4b08ff296a2797b8cb17d0b6706e90814a89
Ilya Ballet <anonimus33@protonmail.com>
2022-05-15 09:01:33 +0300

    reordered properties for consistensy

--- src/server/Projectile.hpp
+++ src/server/Projectile.hpp
@@ -7,20 +7,21 @@ class Projectile
     public:
         bool active;
         bool potato_swap;
+        std::string nickname;
         sf::Int16 x;
         sf::Int16 y;
+        double rotation;
         double damage;
         double range;
         double speed;
-        double rotation;
         double falloff;
         double headshot_multiplier;
         double obstacle_multiplier;
-        std::string nickname;
         Delegate action;
 
         void move();
-        void init(const std::string &nickname,
+        void init(bool potato_swap,
+                  const std::string &nickname,
                   const sf::Int16 &x,
                   const sf::Int16 &y,
                   double rotation,
@@ -30,10 +31,10 @@ class Projectile
                   double falloff,
                   double headshot_multiplier,
                   double obstacle_multiplier,
-                  bool potato_swap,
                   Delegate action)
         {
             active = true;
+            this->potato_swap = potato_swap;
             this->nickname = nickname;
             this->x = x;
             this->y = y;
@@ -44,7 +45,6 @@ class Projectile
             this->falloff = falloff;
             this->headshot_multiplier = headshot_multiplier;
             this->obstacle_multiplier = obstacle_multiplier;
-            this->potato_swap = potato_swap;
             this->action = action;
         }
 };
