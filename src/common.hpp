be6f9fe4c3441d01c5d6f7015b3e2494a0428415
Ilya Ballet <anonimus33@protonmail.com>
2022-05-19 22:58:13 +0300

    added noexcept to getDistance

--- src/common.hpp
+++ src/common.hpp
@@ -38,7 +38,7 @@ namespace fs = std::filesystem;
 namespace surv
 {
     inline static
-    float getDistance(float x1, float x2, float y1, float y2)
+    float getDistance(float x1, float x2, float y1, float y2) noexcept
     {
         return std::sqrt(std::pow((x1 - x2), 2) + std::pow((y1 - y2), 2));
     }
